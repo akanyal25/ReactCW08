4f112855bb6df0e1bd50f4c10edac57f5e0b53c9
akanyal25 <ayush@Ayushs-MacBook-Pro.local>
2023-12-05 15:43:46 -0500

    first github pages

---
+++ src/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 5
+        };
+    }
+
+    incrementCount = () => {
+        this.setState(prevState =>({
+            count: prevState.count + 1
+        }));
+
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                <h1>{this.state.count}</h1>
+
+                <button onClick={this.incrementCount}>Increment Counter</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
